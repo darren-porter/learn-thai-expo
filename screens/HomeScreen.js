b468eed2556b9a50480c85f1a2d36ba399a4f1e5
Darren Porter <darren_po@dtgsiam.com>
2020-04-19 18:47:04 +0700

    home details

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,68 +1,36 @@
-import React, { useState, useEffect } from "react";
-import { Audio } from "expo-av";
-
+import React from "react";
 import CustomHeader from "../components/CustomHeader";
-import { View, TouchableOpacity, FlatList, StyleSheet } from "react-native";
-import { Content, Text, Button } from "native-base";
-
-const RenderWords = ({ item }) => (
-  <View>
-    <TouchableOpacity style={styles.item} onPress={tryPlays}>
-      <Text>{item.english}</Text>
-      <Text>{item.thai}</Text>
-      <Text>{item.phonetic}</Text>
-    </TouchableOpacity>
-  </View>
-);
-
-async function tryPlays() {
-  const soundObject = new Audio.Sound();
-  try {
-    await soundObject.loadAsync(require("../assets/sounds/hello.mp3"));
-    await soundObject.playAsync();
-    // Your sound is playing!
-  } catch (error) {
-    // An error occurred!
-  }
-}
+import { Content, Text, Card, CardItem, Body } from "native-base";
 
 const HomeScreen = ({ navigation }) => {
-  const [words, setWords] = useState([
-    {
-      english: "hello",
-      thai: "สวัสดี",
-      phonetic: "sà-wàt-dee",
-    },
-    {
-      english: "goodbye",
-      thai: "ลาก่อน",
-      phonetic: "laa gòn",
-    },
-  ]);
-
   return (
     <>
-      <CustomHeader title="Home" isHome={true} />
+      <CustomHeader title="Learn Thai" isHome={true} />
       <Content padder>
-        <Button onPress={() => navigation.navigate("HomeDetail")}>
-          <Text>Go Home Detail</Text>
-        </Button>
-        <FlatList
-          data={words}
-          renderItem={({ item }) => <RenderWords item={item} />}
-          keyExtractor={(item, index) => item + index}
-        />
+        <Card>
+          <CardItem
+            button
+            onPress={() =>
+              navigation.navigate("HomeDetail", {
+                title: "Basics",
+                words: [
+                  {
+                    english: "hello",
+                    thai: "สวัสดี",
+                    audioFile: require("../assets/sounds/hello.mp3"),
+                  },
+                ],
+              })
+            }
+          >
+            <Body>
+              <Text>Basics</Text>
+            </Body>
+          </CardItem>
+        </Card>
       </Content>
     </>
   );
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  item: {
-    padding: 10,
-    backgroundColor: "lightgray",
-    marginVertical: 5,
-  },
-});
--- screens/HomeScreenDetail.js
+++ screens/HomeScreenDetail.js
@@ -1,17 +1,72 @@
 import React from "react";
+import { Audio } from "expo-av";
 
 import CustomHeader from "../components/CustomHeader";
-import { Content, Text } from "native-base";
+import { View, TouchableOpacity, FlatList, StyleSheet } from "react-native";
+import { Content, Text, Icon } from "native-base";
 
-const HomeScreenDetail = ({ navigation }) => {
+const RenderWords = ({ item }) => (
+  <View>
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => tryPlay(item.audioFile)}
+    >
+      <View>
+        <Text style={styles.itemText}>{item.english}</Text>
+        <Text style={styles.itemText}>{item.thai}</Text>
+      </View>
+      <View>
+        <Icon name="play" />
+      </View>
+    </TouchableOpacity>
+  </View>
+);
+
+const tryPlay = async (audioFile) => {
+  try {
+    const { sound: soundObject, status } = await Audio.Sound.createAsync(
+      audioFile,
+      {
+        shouldPlay: true,
+      }
+    );
+    // Your sound is playing!
+  } catch (error) {
+    // An error occurred!
+  }
+};
+
+const HomeScreenDetail = ({ navigation, route }) => {
+  const { title, words } = route.params;
+
   return (
     <>
-      <CustomHeader title="Home Detail" navigation={navigation} />
+      <CustomHeader title={title} navigation={navigation} />
       <Content padder>
-        <Text>Home Detail</Text>
+        <FlatList
+          data={words}
+          renderItem={({ item }) => <RenderWords item={item} />}
+          keyExtractor={(item, index) => item + index}
+        />
       </Content>
     </>
   );
 };
 
 export default HomeScreenDetail;
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    backgroundColor: "lightgray",
+    marginVertical: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderRadius: 10,
+  },
+  itemText: {
+    fontFamily: "Roboto",
+    fontSize: 20,
+  },
+});
